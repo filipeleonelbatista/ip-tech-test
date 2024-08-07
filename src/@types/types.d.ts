e52f49360c79d4d263a6d6a622c9e3bd65972c21
Filipe Batista <filipe.x@live.com>
2024-08-07 20:02:38 -0300

    feat: criando tipos de atendimento

--- src/@types/types.d.ts
+++ src/@types/types.d.ts
@@ -13,3 +13,20 @@ export interface Patient {
   };
   status: "active" | "inactive";
 }
+
+export interface Atendimento {
+  id: number;
+  id_Paciente: number;
+  dataHora: string;
+  descricao: string;
+  status: 'Ativo' | 'Inativo';
+}
+
+export interface AtendimentoComUsuario {
+  id: number;
+  id_Paciente: number;
+  dataHora: string;
+  descricao: string;
+  status: 'Ativo' | 'Inativo';
+  patient: Patient | null;
+}
